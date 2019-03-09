57c0eb3394d4ae3bb2222067c5a90cdfc3c0b332
novakvova <novakvova@gmail.com>
2019-03-09 14:58:57 +0200

    Begin work site

---
+++ ExampleWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExampleWebApp.Startup))]
+namespace ExampleWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
